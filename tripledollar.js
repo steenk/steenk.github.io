416dfa809241293332e5910b51381d45c57f2a98
Steen Klingberg <steen@sk-ab.se>
2013-09-06 18:56:35 +0200

    complete

--- tripledollar.js
+++ tripledollar.js
@@ -1,2 +1,218 @@
-/* tripledollar v.0.6.4, (c) 2013 Steen Klingberg. License MIT. */
-(function(){var t="0.6.4";var e=function(){var t=Array.prototype.slice.call(arguments);if(typeof t[0]!=="string"){if(Object.prototype.toString.call(t[0])==="[object Array]"){return e.apply(this,t[0])}}var r=t.shift(),n=r.split(/[\.#]/),i=r.split(/[^\.#]+/),a;if(i[0]==""){i.shift()}for(var o=0;o<n.length;o++){if(o===0){a=document.createElement(n[o])}else{if(i[o-1]==="."){a.classList.add(n[o])}else if(i[o-1]==="#"){a.setAttribute("id",n[o])}}}function s(t){for(var r=0;r<t.length;r++){var n=t[r];if(n&&n.nodeType){a.appendChild(n)}else if(typeof n==="object"){if(Object.prototype.toString.call(n)==="[object Array]"){a.appendChild(e.apply(this,n))}else{for(var i in n){if(i.match(/^data./)){var o=i.substr(4).toLowerCase();a.setAttribute("data-"+o,n[i])}else{a.setAttribute(i,n[i])}}}}else{a.appendChild(document.createTextNode(String(n)))}}}s(t);a.css=function(t){for(var e in t){this.style[e]=t[e]}return this};a.set=function(t,e){this[t]=e;return this};a.fun=function(t){var e=Array.prototype.slice.call(arguments,1);this[t].apply(this,e);return this};a.evt=function(t,e){if(arguments.length>2){var r=Array.prototype.slice.call(arguments,2);if(this.addEventListener){this.addEventListener(t,function(t){var n=[t].concat(r);e.apply(this,n)})}else{this.attachEvent("on"+t,function(t){var n=[t].concat(r);e.apply(this,n)})}}else{if(this.addEventListener){this.addEventListener(t,e)}else{this.attachEvent("on"+t,e)}}return this};a.ins=function(){var t=Array.prototype.slice.call(arguments);s(t)};a.$=a.querySelectorAll;a.prototype=a;return a};e.version=t;e.structify=function(t){if(t.nodeType===1){function e(t){var r=[],n=t.localName,i=t.className.replace(" ",".");if(i){n+="."+i}if(t.id){n+="#"+t.id}r.push(n);if(t.hasAttributes()){var a=t.attributes,o={};for(var s=0;s<a.length;s++){if(!a[s].name.match(/id|class|contenteditable/)){o[a[s].name]=a[s].value}}if(Object.keys(o).length>0){r.push(o)}}t.normalize();var l=t.childNodes;for(var s=0;s<l.length;s++){if(l[s].nodeType===3){var c=l[s].data.replace(/\s\s*/," ");if(c.length>0){r.push(c)}}else if(l[s].nodeType===1){r.push(e(l[s]))}}return r}}var r=e(t);return r};e.appendToDoc=function(){var t=Array.prototype.slice.call(arguments);for(var r=0;r<t.length;r++){if(Object.prototype.toString.call(t[r])==="[object Array]"){document.body.appendChild(e(t[r]))}else if(t[r]instanceof HTMLElement){document.body.appendChild(t[r])}}};if(typeof define==="function"){define(function(t){return e})}else{window.$$$=e;window.tripledollar=e.appendToDoc}if(!window.$&&document.querySelectorAll){window.$=function(t){return document.querySelectorAll(t)}}})();
+/*
+ * Copyright (C) 2013 Steen Klingberg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+(function () {
+  var VERSION = '0.6.4';
+/*
+ * The triple dollar function creates a DOM object.
+ */
+  var $$$ = function () {
+    var args = Array.prototype.slice.call(arguments);
+    if (typeof args[0] !== 'string') {
+      if (Object.prototype.toString.call(args[0]) === '[object Array]') {
+        return $$$.apply(this, args[0]);
+      }
+    }
+
+    /*
+     * Splitting up the ident parameter into element type, id, and class names. 
+     */
+    var ident = args.shift()
+     ,  n = ident.split(/[\.#]/)
+     ,  t = ident.split(/[^\.#]+/)
+     ,  e;
+    if (t[0] == '') {
+      t.shift();
+    }
+    for (var i=0; i<n.length; i++) {
+      if (i === 0) {
+        e = document.createElement(n[i]);
+      } else {
+        if (t[i-1] === '.') {
+          e.classList.add(n[i]);
+        } else if (t[i-1] === '#') {
+          e.setAttribute('id', n[i]);
+        }
+      }
+    };
+
+    function allArgs (args) {
+      for (var i=0; i<args.length; i++) {
+        var param = args[i];
+        if (param && param.nodeType) {
+          e.appendChild(param);
+        } else if (typeof param === 'object') {
+          if (Object.prototype.toString.call(param) === '[object Array]') {
+            e.appendChild($$$.apply(this, param));
+          } else {
+            for (var a in param) {
+              if (a.match(/^data./)) {
+                var atr = a.substr(4).toLowerCase();
+                e.setAttribute('data-' + atr, param[a]);
+              } else {
+                e.setAttribute(a, param[a]);
+              }
+            }
+          }
+        } else {
+          e.appendChild(document.createTextNode(String(param)));
+        }
+      }
+    };
+    allArgs(args);
+    /*
+     * Add CSS to the element.
+     */
+    e.css = function (obj) {
+      for (var k in obj) {
+        this.style[k] = obj[k];
+      };
+      return this;
+    };
+    /*
+     * Set a property.
+     */
+    e.set = function (key, val) {
+      this[key] = val;
+      return this;
+    };
+    /*
+     * Run a function.
+     */
+    e.fun = function (func) {
+      var args = Array.prototype.slice.call(arguments, 1);
+      this[func].apply(this, args);
+      return this;
+    };
+    /*
+     * Add event listener
+     */
+    e.evt = function (ev, func) {
+      if (arguments.length > 2) {
+        var args = Array.prototype.slice.call(arguments, 2);
+        if (this.addEventListener) {
+          this.addEventListener(ev, function (e) {var a = [e].concat(args); func.apply(this, a)});
+        } else {
+          this.attachEvent('on'+ev, function (e) {var a = [e].concat(args); func.apply(this, a)});
+        }
+      } else {
+        if (this.addEventListener) {
+          this.addEventListener(ev, func);
+        } else {
+          this.attachEvent('on'+ev, func);
+        }
+      }
+      return this;
+    };
+    /*
+     * Insert more things to this element
+     */
+    e.ins = function () {
+      var args = Array.prototype.slice.call(arguments);
+      allArgs(args);
+    };
+    /*
+     * Add an alias for a CSS selector
+     */
+    e.$ = e.querySelectorAll;
+    e.prototype = e;
+    return e;
+  };
+
+  $$$.version = VERSION;
+
+  /*
+   * Structify an element node
+   */
+  $$$.structify = function (elem) {
+    if (elem.nodeType === 1) {
+      function dig (c) {
+        var l = []
+         ,  name = c.localName
+         ,  cname = c.className.replace(' ', '.');
+        if (cname) {
+          name += '.'+cname;
+        };
+        if (c.id) {
+          name += '#'+c.id;
+        };
+        l.push(name);
+        if (c.hasAttributes()) {
+          var attrs = c.attributes
+           ,  attr = {};
+          for (var i=0; i<attrs.length; i++) {
+            if (!attrs[i].name.match(/id|class|contenteditable/)) {
+              attr[attrs[i].name] = attrs[i].value;              
+            }
+          }
+          if (Object.keys(attr).length > 0) {
+            l.push(attr);
+          }
+        }
+        c.normalize();
+        var ch = c.childNodes;
+        for (var i=0; i<ch.length; i++) {
+          if(ch[i].nodeType === 3) {
+            var s = ch[i].data.replace(/\s\s*/,' ');
+            if (s.length > 0) {
+              l.push(s);
+            }
+          } else if (ch[i].nodeType === 1) {
+            l.push(dig(ch[i]));
+          }
+        }
+        return l;
+      }
+    }
+    var td = dig(elem);
+    return td;
+  }
+
+  /* 
+   * A shortcut for placing the content on the web page.
+   */
+  $$$.appendToDoc = function () {
+    var args = Array.prototype.slice.call(arguments);
+    for (var i=0; i<args.length; i++) {
+      if (Object.prototype.toString.call(args[i]) === '[object Array]') {
+        document.body.appendChild($$$(args[i]));
+      } else if (args[i] instanceof HTMLElement) {
+        document.body.appendChild(args[i]);
+      }
+    }
+  }
+
+  /*
+   * Use AMD if a module loader is in place.
+   */
+  if (typeof define === 'function') {
+    define(function (require) {
+      return $$$;
+    }) 
+  } else {
+    window.$$$ = $$$;
+    window.tripledollar = $$$.appendToDoc;
+  }
+
+  /*
+   * In case a $ function is not initialized.
+   */
+  if (! window.$ && document.querySelectorAll) {window.$ = function (sel) { return document.querySelectorAll(sel);}};
+
+})();
